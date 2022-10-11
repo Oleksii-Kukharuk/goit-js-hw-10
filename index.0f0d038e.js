abf88e5f376a55289ef02740bd30ef4c48db0615
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-10-11 20:34:46 +0000

    Deploying to gh-pages from @ Oleksii-Kukharuk/goit-js-hw-10@b713ceef6a09a7383895da0ae41e31c90f7dbb52 🚀

---
+++ index.0f0d038e.js
@@ -0,0 +1,2 @@
+var t;fetch(`https://restcountries.com/v3.1/name/${t}`);
+//# sourceMappingURL=index.0f0d038e.js.map
---
+++ index.8164bf55.js
@@ -0,0 +1,2 @@
+!function(){var t;fetch("https://restcountries.com/v3.1/name/".concat(t))}();
+//# sourceMappingURL=index.8164bf55.js.map
